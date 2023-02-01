f521a91749dd2957c55249846ce00378c38fd7c2
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2023-02-01 09:46:02 +0530

    Reduce number of chunks retrieved from search.

--- knowledge_gpt/utils.py
+++ knowledge_gpt/utils.py
@@ -105,7 +105,7 @@ def search_docs(index: VectorStore, query: str) -> List[Document]:
     and returns a list of Documents."""
 
     # Search for similar chunks
-    docs = index.similarity_search(query, k=8)
+    docs = index.similarity_search(query, k=5)
     return docs
 
 
