187d8a7d889498759d573faafe40623362eb63f1
Sasmitha Manathunga <70096033+mmz-001@users.noreply.github.com>
2023-02-10 11:48:33 +0530

    [Refactor] move sidebar to components (#11)

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from components.sidebar import sidebar
 from utils import (
     parse_docx,
     parse_pdf,
@@ -17,44 +18,10 @@ def clear_submit():
     st.session_state["submit"] = False
 
 
-def set_openai_api_key(api_key: str):
-    st.session_state["OPENAI_API_KEY"] = api_key
-
-
 st.set_page_config(page_title="KnowledgeGPT", page_icon="📖", layout="wide")
 st.header("📖KnowledgeGPT")
 
-with st.sidebar:
-    st.markdown("# About")
-    st.markdown(
-        "📖KnowledgeGPT allows you to ask questions about your "
-        "documents and get accurate answers with instant citations. "
-    )
-    st.markdown(
-        "This tool is a work in progress. "
-        "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "
-        "with your feedback and suggestions💡"
-    )
-    st.markdown("---")
-    st.markdown(
-        "## How to use\n"
-        "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"
-        "2. Upload a pdf, docx, or txt file📄\n"
-        "3. Ask a question about the document💬\n"
-    )
-    api_key_input = st.text_input(
-        "OpenAI API Key",
-        type="password",
-        placeholder="Paste your OpenAI API key here (sk-...)",
-        help="You can get your API key from https://platform.openai.com/account/api-keys.",
-        value=st.session_state.get("OPENAI_API_KEY", ""),
-    )
-
-    if api_key_input:
-        set_openai_api_key(api_key_input)
-
-    st.markdown("---")
-    st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
+sidebar()
 
 uploaded_file = st.file_uploader(
     "Upload a pdf, docx, or txt file",
