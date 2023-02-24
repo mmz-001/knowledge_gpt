d7708c42163230d697dcf53f5adbe13fb1ce3fa2
Asad-Rizwan <106770564+Asad-Rizwan@users.noreply.github.com>
2023-02-24 22:12:25 +0500

    Update utils.py

--- knowledge_gpt/utils.py
+++ knowledge_gpt/utils.py
@@ -1,20 +1,23 @@
 import re
 from io import BytesIO
 from typing import Any, Dict, List
+
 import docx2txt
 import streamlit as st
 from embeddings import OpenAIEmbeddings
+from langchain import Cohere, OpenAI
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.docstore.document import Document
 from langchain.llms import OpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import VectorStore
+from langchain.vectorstores import FAISS, VectorStore
+from langchain.vectorstores.faiss import FAISS
 from openai.error import AuthenticationError
 from prompts import STUFF_PROMPT
 from pypdf import PdfReader
+
+
 @st.experimental_memo()
-
-
 def parse_docx(file: BytesIO) -> str:
     text = docx2txt.process(file)
     # Remove multiple newlines
@@ -23,8 +26,6 @@ def parse_docx(file: BytesIO) -> str:
 
 
 @st.experimental_memo()
-
-
 def parse_pdf(file: BytesIO) -> List[str]:
     pdf = PdfReader(file)
     output = []
@@ -36,6 +37,7 @@ def parse_pdf(file: BytesIO) -> List[str]:
         text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
         # Remove multiple newlines
         text = re.sub(r"\n\s*\n", "\n\n", text)
+
         output.append(text)
 
     return output
@@ -51,8 +53,7 @@ def parse_txt(file: BytesIO) -> str:
 
 @st.cache(allow_output_mutation=True)
 def text_to_docs(text: str | List[str]) -> List[Document]:
-    """Converts a string or list 
-    of strings to a list of Documents
+    """Converts a string or list of strings to a list of Documents
     with metadata."""
     if isinstance(text, str):
         # Take a single string as one page
@@ -69,36 +70,33 @@ def text_to_docs(text: str | List[str]) -> List[Document]:
     for doc in page_docs:
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=800,
-            separators=["\n\n", "\n", ".", "!",
-                        "?", ",", " ", ""],chunk_overlap=0,
+            separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
+            chunk_overlap=0,
         )
         chunks = text_splitter.split_text(doc.page_content)
         for i, chunk in enumerate(chunks):
             doc = Document(
-                page_content=chunk, 
-                metadata={"page": doc.metadata["page"], "chunk": i}
+                page_content=chunk, metadata={"page": doc.metadata["page"], "chunk": i}
             )
-            
-            doc.metadata["source"] = f"""{doc.metadata['page']}-
-            {doc.metadata['chunk']}"""
+            # Add sources a metadata
+            doc.metadata["source"] = f"{doc.metadata['page']}-{doc.metadata['chunk']}"
             doc_chunks.append(doc)
     return doc_chunks
 
 
-@st.cache(allow_output_mutation=True,
-          show_spinner=False)
-
-
+@st.cache(allow_output_mutation=True, show_spinner=False)
 def embed_docs(docs: List[Document]) -> VectorStore:
     """Embeds a list of Documents and returns a FAISS index"""
 
     if not st.session_state.get("OPENAI_API_KEY"):
-        raise AuthenticationError
-        ("Enter your OpenAI API key in the sidebar.You can get a key at https://platform.openai.com/account/api-keys.")
+        raise AuthenticationError(
+            "Enter your OpenAI API key in the sidebar. You can get a key at https://platform.openai.com/account/api-keys."
+        )
     else:
         # Embed the chunks
-        embeddings = OpenAIEmbeddings(openai_api_key=st.session_state.get("OPENAI_API_KEY"))
+        embeddings = OpenAIEmbeddings(openai_api_key=st.session_state.get("OPENAI_API_KEY"))  # type: ignore
         index = FAISS.from_documents(docs, embeddings)
+
         return index
 
 
@@ -118,21 +116,19 @@ def get_answer(docs: List[Document], query: str) -> Dict[str, Any]:
 
     # Get the answer
 
-    chain = load_qa_with_sources_chain(OpenAI(temperature=0, 
-            openai_api_key=st.session_state.get("OPENAI_API_KEY")), 
-            chain_type="stuff", prompt=STUFF_PROMPT)  # type: ignore
+    chain = load_qa_with_sources_chain(OpenAI(temperature=0, openai_api_key=st.session_state.get("OPENAI_API_KEY")), chain_type="stuff", prompt=STUFF_PROMPT)  # type: ignore
 
     # Cohere doesn't work very well as of now.
     # chain = load_qa_with_sources_chain(Cohere(temperature=0), chain_type="stuff", prompt=STUFF_PROMPT)  # type: ignore
     answer = chain(
-      {"input_documents": docs, "question": query}, return_only_outputs=True
+        {"input_documents": docs, "question": query}, return_only_outputs=True
     )
     return answer
 
 
 @st.cache(allow_output_mutation=True)
 def get_sources(answer: Dict[str, Any], docs: List[Document]) -> List[Document]:
-   
+    """Gets the source documents for an answer."""
 
     # Get sources for the answer
     source_keys = [s for s in answer["output_text"].split("SOURCES: ")[-1].split(", ")]
