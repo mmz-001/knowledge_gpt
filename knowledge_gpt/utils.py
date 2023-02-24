1e9ba138e5a3a1328df8bd93b81188fe0d753508
Asad-Rizwan <106770564+Asad-Rizwan@users.noreply.github.com>
2023-02-24 21:59:54 +0500

    Update utils.py

--- knowledge_gpt/utils.py
+++ knowledge_gpt/utils.py
@@ -84,8 +84,8 @@ def embed_docs(docs: List[Document]) -> VectorStore:
     """Embeds a list of Documents and returns a FAISS index"""
 
     if not st.session_state.get("OPENAI_API_KEY"):
-        raise AuthenticationError("Enter your OpenAI API key in the sidebar. 
-         You can get a key at https://platform.openai.com/account/api-keys.")
+        raise AuthenticationError
+        ("Enter your OpenAI API key in the sidebar.You can get a key at https://platform.openai.com/account/api-keys.")
     else:
         # Embed the chunks
         embeddings = OpenAIEmbeddings(openai_api_key=st.session_state.get("OPENAI_API_KEY"))
