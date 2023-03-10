f6d5144cafbb75da0369a94ee5dd0d71f7eb157e
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2023-03-10 07:38:28 +0530

    add faq section

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+from knowledge_gpt.components.faq import faq
+
 
 def set_openai_api_key(api_key: str):
     st.session_state["OPENAI_API_KEY"] = api_key
@@ -35,5 +37,7 @@ def sidebar():
             "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
             "with your feedback and suggestions💡"
         )
-        st.markdown("---")
         st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
+        st.markdown("---")
+
+        faq()
