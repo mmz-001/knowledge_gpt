213e7baded479ce423c727ddebb73dc886422d68
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2023-07-05 11:26:53 +0530

    extract FakeFile class to fake_file.py

--- tests/test_chunking.py
+++ tests/test_chunking.py
@@ -1,18 +1,8 @@
 import pytest
 from langchain.docstore.document import Document
-from knowledge_gpt.core.parsing import File
 
 from knowledge_gpt.core.chunking import chunk_file
-
-from io import BytesIO
-
-
-class FakeFile(File):
-    """A fake file for testing purposes"""
-
-    @classmethod
-    def from_bytes(cls, file: BytesIO) -> "FakeFile":
-        return NotImplemented
+from tests.core.fake_file import FakeFile
 
 
 @pytest.fixture
